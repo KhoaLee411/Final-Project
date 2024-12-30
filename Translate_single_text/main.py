53b19549e45293be8e3b2d1a218c64a445d510de
kanedinh <inhxuankhang@gmail.com>
2024-12-30 07:41:13 +0700

    fix main.py

--- Translate_single_text/main.py
+++ Translate_single_text/main.py
@@ -24,20 +24,6 @@ model = genai.GenerativeModel(
   generation_config=generation_config,
 )
 
-# Create the model
-generation_config = {
-  "temperature": 1,
-  "top_p": 0.95,
-  "top_k": 40,
-  "max_output_tokens": 8192,
-  "response_mime_type": "text/plain",
-}
-
-model = genai.GenerativeModel(
-  model_name="gemini-2.0-flash-exp",
-  generation_config=generation_config,
-)
-
 def translate_single_text(json):
   chat_session = model.start_chat(
     history=[
